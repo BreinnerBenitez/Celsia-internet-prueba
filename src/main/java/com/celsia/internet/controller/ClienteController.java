233e1a0b143cce5c06b65a76ddea7e91eec5331a
breinner <jbreinner_inge@hotmail.com>
2026-06-05 00:06:05 -0500

    cambios en controller clinetes

--- src/main/java/com/celsia/internet/controller/ClienteController.java
+++ src/main/java/com/celsia/internet/controller/ClienteController.java
@@ -3,42 +3,70 @@ package com.celsia.internet.controller;
 import com.celsia.internet.dto.ClienteDTO;
 import com.celsia.internet.model.Cliente;
 import com.celsia.internet.service.IClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-// Nota los  DTO se pueden  implementar tambien para traer los datos mas seguro por cuestion
+
 @RestController
 @RequestMapping("/api/cliente")
+@Tag(name = "Clientes", description = "Operaciones relacionadas con clientes")
 public class ClienteController {
 
     @Autowired
     private IClienteService clienteService;
 
+    @Operation(
+            summary = "Obtener todos los clientes",
+            description = "Retorna una lista con todos los clientes registrados"
+    )
     @GetMapping
     public ResponseEntity <List<ClienteDTO>> getCliente() {
         return  ResponseEntity.ok(clienteService.getCliente());
     }
 
-    @PostMapping("/crear")
-    public String saveCliente(@RequestBody Cliente cliente) {
+
+
+    @Operation(summary = "Crear un cliente")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Cliente creado"),
+            @ApiResponse(responseCode = "400", description = "Datos inválidos")
+    })
+    @PostMapping
+    public ResponseEntity<?> saveCliente(@RequestBody Cliente cliente) {
         clienteService.saveCliente(cliente);
 
-        return "el cleinte fue creada correctamente";
+        return ResponseEntity.status(HttpStatus.CREATED).body("cliente creado");
     }
 
-    @DeleteMapping("/borrar/{id}")
-    public String deleteCliente(@PathVariable Long id) {
+
+    @Operation(
+            summary = "Eliminar un cliente",
+            description = "Elimina un cliente según su ID"
+    )
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCliente(@PathVariable Long id) {
         clienteService.deleteCliente(id);
-        return "el cliente fue eliminada correctamente";
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @PutMapping("/editar")
-    public Cliente editPersona(@RequestBody Cliente cliente) {
+    @Operation(
+            summary = "Editar un cliente",
+            description = "Actualiza los datos de un cliente existente"
+    )
+    @PutMapping("/{id}")
+    public ResponseEntity <Cliente> editCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
+        cliente.setIdentificacion(id);
         clienteService.editCliente(cliente);
 
-        return clienteService.findCliente(cliente.getIdentificacion());
+        return   ResponseEntity.noContent().build();
     }
 
 
